f7fc74d7cf2a402ddd1b4ccbce9e1feb9f9a4ebc
Poczok <tamaspece@gmail.com>
2019-04-30 15:34:21 +0200

    Pecze wrote about 30 lines of code

---
+++ memorygame/src/Card.java
@@ -0,0 +1,25 @@
+public class Card {
+    private int value;
+    private boolean isFound;
+
+    public Card(int value, boolean isFound) {
+        this.value = value;
+        this.isFound = isFound;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isFound() {
+        return isFound;
+    }
+
+    public void setFound(boolean found) {
+        isFound = found;
+    }
+}
